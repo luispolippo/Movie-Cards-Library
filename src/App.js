78639eabd113f7cc8e21c030c1ba50dfee78476a
Guilherme Polippo <luispolippo@hotmail.com>
2021-12-27 15:18:15 -0300

    Projeto Movies Cards Library

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/Header';
+import MovieList from './components/MovieList';
+import data from './data';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <MovieList movies={ data } />
+      </div>
+    );
+  }
+}
+
+export default App;
